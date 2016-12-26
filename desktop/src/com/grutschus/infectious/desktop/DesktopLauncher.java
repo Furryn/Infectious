d5c5ffd781dab026ee2bfde6122f59768058a1d0
Till <hsv_fan_till@hotmail.de>
2016-12-26 14:29:42 +0100

    Initial commit

---
+++ desktop/src/com/grutschus/infectious/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.grutschus.infectious.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.grutschus.infectious.Infectious;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Infectious(), config);
+	}
+}
