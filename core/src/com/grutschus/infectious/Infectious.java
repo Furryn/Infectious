f3e06d8a10e3c0d7f78199f8e133fee9c02d1e08
Till <hsv_fan_till@hotmail.de>
2016-12-29 16:51:29 +0100

    Input Handling prep

--- core/src/com/grutschus/infectious/Infectious.java
+++ core/src/com/grutschus/infectious/Infectious.java
@@ -5,42 +5,37 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.grutschus.infectious.input.InputHandler;
 
 public class Infectious extends ApplicationAdapter {
-	private static SpriteBatch batch;
 	private static Stage stage;
-	private static Camera camera;
 	private static Viewport viewport;
-
-	private static Texture texture;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		camera = new OrthographicCamera();
+		SpriteBatch batch = new SpriteBatch();
+		Camera camera = new OrthographicCamera();
 		viewport = new FitViewport(CoreReference.GAMEWORLD_WIDTH, CoreReference.GAMEWORLD_HEIGHT, camera);
 		stage = new Stage(viewport, batch);
 
-		texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+		Gdx.input.setInputProcessor(new InputHandler());
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(texture, 0, 0);
-		batch.end();
+		stage.act();
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		stage.dispose();
 	}
 
 	@Override
---
+++ core/src/com/grutschus/infectious/input/Controls.java
@@ -0,0 +1,17 @@
+package com.grutschus.infectious.input;
+
+import com.badlogic.gdx.Input;
+
+public abstract class Controls {
+
+    static int KEY_MOVE_UP = Input.Keys.W;
+    static int KEY_MOVE_DOWN = Input.Keys.S;
+    static int KEY_MOVE_LEFT = Input.Keys.A;
+    static int KEY_MOVE_RIGHT = Input.Keys.D;
+
+    static int KEY_ATTACK_1 = Input.Keys.J;
+    static int KEY_ATTACK_2 = Input.Keys.K;
+    static int KEY_ATTACK_3 = Input.Keys.L;
+    static int KEY_ATTACK_4 = Input.Keys.SEMICOLON;
+
+}
